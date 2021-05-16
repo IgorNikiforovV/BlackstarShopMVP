fa92ab6ca8c7ba52146cb01b7046270f973445f8
Igor <igor.nikiforov@domru.ru>
2021-05-16 18:19:56 +0500

    Added folders structure and tabBar icons

---
+++ BlackstarShopMVP/Screens/MainTabBar/MainTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabBarController.swift
+//  BlackstarShopMVP
+//
+//  Created by Игорь Никифоров on 16.05.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+
+
+}
